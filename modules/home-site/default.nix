62a957d8e488c071c2116935d322f1ce9e9bd26c
Maxwell Schaefer <max@theschaefers.com>
2025-10-05 21:55:23 -0700

    Fixed nginx conf

--- modules/home-site/default.nix
+++ modules/home-site/default.nix
@@ -294,10 +294,13 @@ in
           proxyPass = "http://127.0.0.1:${toString cfg.listenPort}";
           proxyWebsockets = true;
           extraConfig = ''
-            proxy_set_header Host $host;
+            proxy_set_header Host ${cfg.domain};
+            proxy_set_header X-Forwarded-Host $host;
+            proxy_set_header X-Forwarded-Port $server_port;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_redirect off;
           '' + cfg.nginx.extraConfig;
         };
       };
